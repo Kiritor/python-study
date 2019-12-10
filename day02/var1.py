65c7589a1dfc1945a3310758e7ecde21b7ef108d
tao.liang <1406754158@qq.com>
2019-12-10 10:07:17 +0800

    s

---
+++ day02/var1.py
@@ -0,0 +1,16 @@
+"""
+使用变量保存数据并进行基本操作
+
+Version: 0.1
+Author: Kiritor
+Date: 2019-12-10
+"""
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b) # 模运算
+print(a % b)  # 取余
+print(a ** b) # 指数运算
